b18c6bd2d73eff5d629c6b4a02131a91054e5ae2
Anastasia Gorlina <gobi199@yandex.ru>
2023-05-06 13:25:28 +0300

    add function

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const form = document.querySelector('form');
+  const form = document.querySelector('form');
+  const errorMessage = document.getElementById('error-message');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const inputs = event.target.elements;
 
-  const data = new FormData(event.target);
-  const jsonData = JSON.stringify(Object.fromEntries(data.entries()));
+    // проверяем день
+    const selectedDate = new Date(inputs.date.value);
+    const currentDate = new Date();
+    if (selectedDate < currentDate) {
+      errorMessage.textContent = 'Этот день уже прошёл';
+      errorMessage.classList.add('error');
+      return;
+    }
 
-  console.log(jsonData);
-});
+    // проверяем время
+    const timeStart = inputs.timeStart.value;
+    const timeEnd = inputs.timeEnd.value;
+    if (timeStart >= timeEnd) {
+      errorMessage.textContent = 'Время окончания раньше времени начала';
+      errorMessage.classList.add('error');
+      return;
+    } else {
+      errorMessage.textContent = '';
+      errorMessage.classList.remove('error');
+    }
 
-form.addEventListener('reset', event => {
-  event.preventDefault();
+  // проверяем, заполнены ли все поля, кроме comment
+  if (inputs.date.value.trim() === '' || inputs.timeStart.value.trim() === '' || inputs.timeEnd.value.trim() === '') {
+    errorMessage.textContent = 'Пожалуйста, заполните все поля';
+    errorMessage.classList.add('error');
+    return;
+  }
 
-  form.reset();
-});
+    const data = new FormData(event.target);
+    const jsonData = JSON.stringify(Object.fromEntries(data.entries()));
+
+    console.log(jsonData);
+  });
+
+  const resetButton = document.querySelector('button[type="reset"]');
+  resetButton.addEventListener('click', () => {
+    form.reset();
+  });
