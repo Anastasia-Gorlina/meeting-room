1f3ec5cfc45825c9a7497eec955ab1ac91a513f3
Anastasia Gorlina <gobi199@yandex.ru>
2023-05-07 15:14:48 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -8,32 +8,39 @@
 
       // проверяем, заполнены ли все поля, кроме comment
   if (inputs.date.value.trim() === '' || inputs.timeStart.value.trim() === '' || inputs.timeEnd.value.trim() === '') {
-    errorMessage.textContent = 'Пожалуйста, заполните все поля';
-    errorMessage.classList.add('error');
+    inputs.date.classList.add('error');
+    inputs.timeStart.classList.add('error');
+    inputs.timeEnd.classList.add('error');
     return;
+  } else {
+    inputs.date.classList.remove('error');
+    inputs.timeStart.classList.remove('error');
+    inputs.timeEnd.classList.remove('error');
   }
 
     // проверяем день
     const selectedDate = new Date(inputs.date.value);
     const currentDate = new Date();
     if (selectedDate < currentDate) {
-      errorMessage.textContent = 'Этот день уже прошёл';
-      errorMessage.classList.add('error');
+      inputs.date.classList.add('error');
       return;
+    } else {
+      inputs.date.classList.remove('error');
     }
 
     // проверяем время
     const timeStart = inputs.timeStart.value;
     const timeEnd = inputs.timeEnd.value;
     if (timeStart >= timeEnd) {
-      errorMessage.textContent = 'Время окончания раньше времени начала';
-      errorMessage.classList.add('error');
+      inputs.timeStart.classList.add('error');
+      inputs.timeEnd.classList.add('error');
       return;
     } else {
-      errorMessage.textContent = '';
-      errorMessage.classList.remove('error');
+      inputs.timeStart.classList.remove('error');
+      inputs.timeEnd.classList.remove('error');
     }
 
+    errorMessage.textContent = '';
     const data = new FormData(event.target);
     const jsonData = JSON.stringify(Object.fromEntries(data.entries()));
 
